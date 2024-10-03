a9e65fc588948a8d6e7966f685a9091ecf31411c
reham0000 <amsarkerreham@gmail.com>
2024-10-03 13:44:02 +0600

    added video title and video duration using API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
 // 1. fetch, load and show categories on html
 
 const loadCategories = () => {
@@ -15,23 +23,24 @@ const loadVideos = () => {
 };
 
 const cardDemo = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
+  category_id: "1001",
+  video_id: "aaaa",
+  thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+  title: "Shape of You",
+  authors: [
+    {
+      profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+      profile_name: "Olivia Mitchell",
+      verified: "",
     },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}; 
+  ],
+  others: {
+    views: "100K",
+    posted_date: "16278",
+  },
+  description:
+    "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
+};
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("videos");
@@ -40,24 +49,45 @@ const displayVideos = (videos) => {
     console.log(video);
 
     const card = document.createElement("div");
-    card.classlist = 'card card-compact';
+    card.classlist = "card card-compact";
     card.innerHTML = `
             
-  <figure>
+  <figure class = "h-[200px] relative">
     <img
       src=${video.thumbnail}
+      class = "h-full w-full object-cover"
       alt="Shoes" />
+      ${
+        video.others.posted_date?.length === 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 text-white bg-black rounded p-1">${getTimeString(
+              video.others.posted_date
+            )}</span>`
+      }
+      
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+      <img class="w-10 h-10 rounded-full object-cover" src=${
+        video.authors[0].profile_picture
+      } />
+    </div>
+    <div>
+      <h2 class="font-bold">${video.title}</h2>
+      <div class="flex items-center gap-2">
+        <p class="text-gray-400">${video.authors[0].profile_name}</p>
+        ${
+          video.authors[0].verified === true
+            ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" />`
+            : ""
+        }
+      </div>
+      
+      <p></p>
     </div>
   </div>
-</div>
         `;
-        videoContainer.append(card);
+    videoContainer.append(card);
   });
 };
 
@@ -77,4 +107,3 @@ const displayCategories = (categories) => {
 
 loadCategories();
 loadVideos();
-// displayCategories();
